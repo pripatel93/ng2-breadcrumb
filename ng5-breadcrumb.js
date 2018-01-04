2d29649c1c099be287f1a4852e1dcfb4e1558fba
Pri Patel <pripatel93@gmail.com>
2018-01-04 11:39:04 +0000

    Added export files

---
+++ ng5-breadcrumb.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_module_1 = require("./app/app.module");
+exports.Ng5BreadcrumbModule = app_module_1.Ng5BreadcrumbModule;
+var breadcrumb_1 = require("./app/components/breadcrumb");
+exports.BreadcrumbComponent = breadcrumb_1.BreadcrumbComponent;
+var breadcrumbService_1 = require("./app/components/breadcrumbService");
+exports.BreadcrumbService = breadcrumbService_1.BreadcrumbService;
+//# sourceMappingURL=ng5-breadcrumb.js.map
---
+++ ng5-breadcrumb.ts
@@ -0,0 +1,3 @@
+export {Ng5BreadcrumbModule} from './app/app.module';
+export {BreadcrumbComponent} from './app/components/breadcrumb';
+export {BreadcrumbService} from './app/components/breadcrumbService';
